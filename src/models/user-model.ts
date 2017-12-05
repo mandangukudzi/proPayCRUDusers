ae48c863fa38edd92bb75b5db6d9471e4b65a774
Mandangu Kudzanayi <mandangu.kudzi@gmail.com>
2017-12-05 20:55:16 +0200

    Added pages/models/providers

---
+++ src/models/user-model.ts
@@ -0,0 +1,28 @@
+export class User{
+//Other details which will appear in the user detail page
+    id: number;
+    name: string;
+    surname: string;
+    display_name: string;
+    email: string;
+    password: string;
+    id_number: string;
+    avatar: string;
+    cell: string;
+    fax: string;
+    skype: string;
+    language_id: string;
+    status_id: number;
+    unread_notices: number;
+    verified: string;
+    last_login: string;
+    remember_token: string;
+    deleted_at: string;
+    created_at: string;
+    updated_at: string;
+    legacy_user_id: number;
+    legacy_level: number;
+    legacy_recruiter_id: number;
+    $key: number;
+
+}
