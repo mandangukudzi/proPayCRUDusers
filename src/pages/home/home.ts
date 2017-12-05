ce7beecb167d75bb3bdbdfb13802ef330098813e
Mandangu Kudzanayi <mandangu.kudzi@gmail.com>
2017-12-05 22:23:02 +0200

    Added functions and AddUser Page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+//local imports
+import { User } from '../../models/user-model';
+
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  user: User;
+  
+  constructor(public navCtrl: NavController) { }
 
-  constructor(public navCtrl: NavController) {
+  ionViewDidLoad(){}
 
+  addUser(): void {
+    this.navCtrl.push("AddUserPage");
   }
 
-  addUser(){}
+  renameUser(user): void { }
 
-  viewUser(user){}
+  viewUser(user): void {
+    console.log("view user");
+  }
 
+  removeUser(user): void {}
+
+  save(){}
 }
